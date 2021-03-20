3ea6263f01647b9d63f0c1be6d4ad0874287dfa7
Lepo00 <luca.leporati@outlook.com>
2021-03-20 09:57:30 +0100

    create empty features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [{ path: 'quiz', loadChildren: () => import('./features/quiz/quiz.module').then(m => m.QuizModule) }];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
+  { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule) }
+  { path: 'quiz', loadChildren: () => import('./features/quiz/quiz.module').then(m => m.QuizModule) },
+  { path: 'create-quiz', loadChildren: () => import('./features/create-quiz/create-quiz.module').then(m => m.CreateQuizModule) },
+  { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
