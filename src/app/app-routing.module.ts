bab6be46231894173b3362e19f0705052db353d1
Lepo00 <luca.leporati@outlook.com>
2021-03-22 18:48:11 +0100

    results page init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ const routes: Routes = [
   { path: 'quiz', loadChildren: () => import('./features/quiz/quiz.module').then(m => m.QuizModule) },
   { path: 'create-quiz', loadChildren: () => import('./features/create-quiz/create-quiz.module').then(m => m.CreateQuizModule) },
   { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'results', loadChildren: () => import('./features/results/results.module').then(m => m.ResultsModule) },
 ];
 
 @NgModule({
