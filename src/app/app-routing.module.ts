cde8c229befc1cdd7d1e035673fffe3ca0afe325
contilorenzo <lorenzo.conti.bg@gmail.com>
2021-03-25 21:49:44 +0100

    css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
+  { path: '', redirectTo: '/quiz', pathMatch: 'full'},
   { path: 'login', loadChildren: () => import('./features/login/login.module').then(m => m.LoginModule) },
   { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule) },
   { path: 'quiz', loadChildren: () => import('./features/quiz/quiz.module').then(m => m.QuizModule) },
