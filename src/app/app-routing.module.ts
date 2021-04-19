303176507d3b909e8bec221794c064aeae322597
Lepo00 <luca.leporati@outlook.com>
2021-04-19 18:42:25 +0200

    finish

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: 'signup', loadChildren: () => import('./features/signup/signup.module').then(m => m.SignupModule) },
   { path: 'quiz', loadChildren: () => import('./features/quiz/quiz.module').then(m => m.QuizModule) },
   { path: 'create-quiz', loadChildren: () => import('./features/create-quiz/create-quiz.module').then(m => m.CreateQuizModule) },
-  { path: 'admin', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
+  { path: 'home', loadChildren: () => import('./features/admin/admin.module').then(m => m.AdminModule) },
   { path: 'results', loadChildren: () => import('./features/results/results.module').then(m => m.ResultsModule) },
   { path: 'contact', loadChildren: () => import('./features/contact/contact.module').then(m => m.ContactModule) },
 ];
